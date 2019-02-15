f2541947201c69cdcdf6a8128363fc3adf58aec9
RMR54 <mrrajalekha@gmail.com>
2019-02-15 15:25:16 -0500

    Removing scale bar

--- src/PlusDataCollection/WinProbe/vtkPlusWinProbeVideoSource.cxx
+++ src/PlusDataCollection/WinProbe/vtkPlusWinProbeVideoSource.cxx
@@ -464,6 +464,8 @@ PlusStatus vtkPlusWinProbeVideoSource::InternalStartRecording()
     return PLUS_FAIL;
   }
   WPSetSize(m_LineCount, m_SamplesPerLine);
+  WPDXSetDrawTextLayer(false);
+  WPDXSetDrawScalesAndBars(false);
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
   if (m_MModeEnabled)
